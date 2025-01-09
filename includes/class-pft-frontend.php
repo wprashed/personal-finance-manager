a7a8e18487a2da47d9f33b902b2c898081111a6c
Md Rashed Hossain <wprashed@icloud.com>
2025-01-09 15:20:54 +0600

    Update class-pft-frontend.php

--- includes/class-pft-frontend.php
+++ includes/class-pft-frontend.php
@@ -20,11 +20,6 @@ class PFT_Frontend {
             wp_enqueue_style('pft-frontend-styles', PFT_PLUGIN_URL . 'assets/css/pft-frontend-styles.css', array(), PFT_VERSION);
             wp_enqueue_script('chart-js', 'https://cdn.jsdelivr.net/npm/chart.js', array(), '3.7.1', true);
             wp_enqueue_script('pft-frontend-scripts', PFT_PLUGIN_URL . 'assets/js/pft-frontend-scripts.js', array('jquery', 'chart-js'), PFT_VERSION, true);
-            
-            wp_localize_script('pft-frontend-scripts', 'pftFrontend', array(
-                'ajaxurl' => admin_url('admin-ajax.php'),
-                'nonce' => wp_create_nonce('pft_frontend_nonce')
-            ));
         }
     }
 
